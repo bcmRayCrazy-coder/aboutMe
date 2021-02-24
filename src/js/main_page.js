8cb8f9dc0b3cfb92643d8a503ae05af154a2ca4e
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2021-02-24 21:06:42 +0800

    add login page

--- src/js/main_page.js
+++ src/js/main_page.js
@@ -15,6 +15,7 @@ Waves.attach('button', ['waves-button', 'waves-float']);
 Waves.attach('#info_box', 'waves-light');
 
 var finder_active = false;
+var login_active = true;
 
 function show_finder() {
     var shower = document.getElementById('info_box');
@@ -28,3 +29,24 @@ function show_finder() {
         finder_active = true;
     }
 }
+
+function show_login() {
+    var shower = document.getElementById('login_box');
+    if (login_active) {
+        shower.classList.remove('show');
+        shower.classList.add('hide');
+        login_active = false;
+    } else {
+        shower.classList.remove('hide');
+        shower.classList.add('show');
+        login_active = true;
+    }
+}
+
+function bind_login() {
+    var app = angular.module('loginApp', []);
+    app.controller('loginCtrl', function($scope) {
+        $scope.nickname = "";
+    });
+}
+bind_login();
